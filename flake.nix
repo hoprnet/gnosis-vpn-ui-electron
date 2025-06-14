684829425ebdcf7cf6b9f39c67c3f772db80584a
Tino Breddin <tolbrino@users.noreply.github.com>
2025-06-14 15:20:22 +0200

    Apply suggestions from code review
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "gnosis-vpn-ui-electron";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+  inputs.nixpkgs.url   = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.simpleFlake {
