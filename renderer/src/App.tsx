ecfcfafb8495a85360dbee20f6f87ee1b7599d57
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2025-06-15 10:04:46 +0200

    wip

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -40,9 +40,9 @@ function App() {
         </div>
         <div className="flex flex-col items-center relative z-10 w-full h-full">
           <Status ref={statusRef} />
-          {/* {status === 'connected' && <Route stops={['Germany', 'Spain']} />}
-          <StartButton /> */}
-          <ManageRouteModal open={true} setOpen={() => {}} />;
+          {/* {status === 'connected' && <Route stops={['Germany', 'Spain']} />} */}
+          <StartButton />
+          {/* <ManageRouteModal open={true} setOpen={() => {}} />; */}
         </div>
       </div>
     </>
